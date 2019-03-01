8978cc6e5461baeaa93378c49d6320ed8b3bb527
Yaron Koren <yaron57@gmail.com>
2019-03-01 14:41:32 +0000

    Add Special:MintyDocsDelete
    Change-Id: I30eedc55aebf2a3eab134a044d8ec706dd375b95

---
+++ includes/MintyDocsDeletePageJob.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * Job to delete a page, for use by Special:MintyDocsDelete.
+ *
+ * @author Yaron Koren
+ */
+class MintyDocsDeletePageJob extends Job {
+
+	function __construct( $title, $params = '', $id = 0 ) {
+		parent::__construct( 'MDDeletePage', $title, $params, $id );
+	}
+
+	/**
+	 * Run an MDDeletePage job.
+	 *
+	 * @return bool success
+	 */
+	function run() {
+		if ( is_null( $this->title ) ) {
+			$this->error = "MDDeletePage: Invalid title";
+			return false;
+		}
+
+		$wikiPage = new WikiPage( $this->title );
+		if ( !$wikiPage ) {
+			$this->error = 'MDDeletePage: Wiki page not found "' . $this->title->getPrefixedDBkey() . '"';
+			return false;
+		}
+
+		$user = User::newFromId( $this->params['user_id'] );
+		$deletionReason = '';
+		if ( array_key_exists( 'deletion_reason', $this->params ) ) {
+			$deletionReason = $this->params['deletion_reason'];
+		}
+
+		$error = '';
+		$wikiPage->doDeleteArticle( $deletionReason, false, null, null, $error, $user );
+		if ( $error != '' ) {
+			$this->error = 'MDDeletePage: ' . $error;
+			return false;
+		}
+
+		return true;
+	}
+}
---
+++ specials/MintyDocsDelete.php
@@ -0,0 +1,136 @@
+<?php
+
+/**
+ * @author Yaron Koren
+ */
+
+class MintyDocsDelete extends SpecialPage {
+
+	/**
+	 * Constructor
+	 */
+	function __construct() {
+		parent::__construct( 'MintyDocsDelete' );
+	}
+
+	function execute( $query ) {
+		$this->setHeaders();
+		$out = $this->getOutput();
+		$req = $this->getRequest();
+
+		// Check permissions.
+		if ( !$this->getUser()->isAllowed( 'mintydocs-administer' ) ) {
+			$this->displayRestrictionError();
+			return;
+		}
+
+		if ( $query == null ) {
+			$out->addHTML( 'Page name must be set.' );
+			return;
+		}
+
+		// Generate Title
+		if ( $query instanceof Title ) {
+			$title = $query;
+		} else {
+			$title = Title::newFromText( $query );
+		}
+
+		// Error if page does not exist
+		if ( !$title->exists() ) {
+			$out->addHTML( 'Page does not exist!' );
+			return;
+		}
+
+		$mdPage = MintyDocsUtils::pageFactory( $title );
+		// For now, this only supports deleting manuals.
+		if ( $mdPage == null || $mdPage->getPageTypeValue() !== 'Manual' ) {
+			$out->addHTML( 'Page must be a MintyDocs manual.' );
+			return;
+		}
+
+		$delete = $req->getCheck( 'mdDelete' );
+		if ( $delete ) {
+			// Guard against cross-site request forgeries (CSRF).
+			$validToken = $this->getUser()->matchEditToken( $req->getVal( 'csrf' ), $this->getName() );
+			if ( !$validToken ) {
+				$text = "This appears to be a cross-site request forgery; canceling.";
+				$out->addHTML( $text );
+				return;
+			}
+
+			$this->deleteAll( $title );
+			return;
+		}
+
+		// display checkbox
+		$text = '<form id="mdDeleteForm" action="" method="post">';
+		$out->addHTML( '<p>The following pages will be deleted:</p>' );
+		$pagesTree = MintyDocsPublish::makePagesTree( $mdPage );
+		$text .= '<ul>';
+		$text .= self::displayTree( $pagesTree['node'], $pagesTree['tree'] );
+		$text .= '</ul>';
+
+		$mdp = $this->getTitle();
+		$text .= Html::hidden( 'title', PFUtils::titleURLString( $mdp ) );
+
+		$text .= "\t" . Html::hidden( 'csrf', $this->getUser()->getEditToken( $this->getName() ) ) . "\n";
+
+		$text .= Html::element( 'input',
+			array(
+				'type' => 'submit',
+				'name' => 'mdDelete',
+				'value' => 'Delete'
+			)
+		);
+
+		$text .= '</form>';
+		$out->addHTML( $text );
+	}
+
+	static function displayTree( $node, $tree ) {
+		$text = '';
+		if ( $node != null ) {
+			$text .= "\n<li>" . $node->getLink() . '</li>';
+		}
+		if ( count( $tree ) > 0 ) {
+			$text .= '<ul>';
+			foreach ( $tree as $node ) {
+				$innerNode = $node['node'];
+				$innerTree = $node['tree'];
+				$text .= self::displayTree( $innerNode, $innerTree );
+			}
+			$text .= '</ul>';
+		}
+		return $text;
+	}
+
+	function deleteAll( $title ) {
+		$user = $this->getUser();
+		$out = $this->getOutput();
+
+		$mdPage = MintyDocsUtils::pageFactory( $title );
+
+		$jobs = array();
+
+		$params = array(
+			'user_id' => $user->getId(),
+			'deletion_reason' => 'Delete manual'
+		);
+
+		$jobs[] = new MintyDocsDeletePageJob( $title, $params );
+		// If this special page ever supports deleting anything other
+		// than manuals (i.e. versions or products), this code will
+		// have to become slightly more complex.
+		foreach ( $mdPage->getChildrenPages() as $child ) {
+			$jobs[] = new MintyDocsDeletePageJob( $child, $params );
+		}
+
+		JobQueueGroup::singleton()->push( $jobs );
+
+		$text = 'The pages will be deleted.';
+
+		$out->addHTML( $text );
+	}
+
+}
