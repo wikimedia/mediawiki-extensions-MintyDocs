923c8590407c11307a17d5cad9d3e4ef0864596e
zoranzoki21 <zorandori4444@gmail.com>
2020-04-01 22:37:24 +0200

    MintyDocsNamespaces.php: Add translations for Serbian
    See configuration in operations/mediawiki-config.
    
    Change-Id: I1eefd56df6b574bc465d2c0ea095aa2c29f205c7

--- MintyDocsNamespaces.php
+++ MintyDocsNamespaces.php
@@ -17,3 +17,15 @@ $namespaceNames['en'] = [
 	MD_NS_DRAFT      => 'Draft',
 	MD_NS_DRAFT_TALK => 'Draft_talk',
 ];
+
+/* Serbian Cyrillic (српски (ћирилица)) */
+$namespaceNames['sr-ec'] = [
+	MD_NS_DRAFT      => 'Нацрт',
+	MD_NS_DRAFT_TALK => 'Разговор_о_нацрту',
+];
+
+/* Serbian Latin (srpski (latinica)) */
+$namespaceNames['sr-el'] = [
+	MD_NS_DRAFT      => 'Nacrt',
+	MD_NS_DRAFT_TALK => 'Razgovor_o_nacrtu',
+];
