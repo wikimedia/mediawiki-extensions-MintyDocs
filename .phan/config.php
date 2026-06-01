0c964b64a62cce1ce6fcbbea547c21aef8614f87
Yaron Koren <yaron57@gmail.com>
2026-06-01 19:02:54 +0000

    Add Phan handling
    Change-Id: I557aabda451182d6ab0be52a83c196a7ec827380

---
+++ .phan/config.php
@@ -0,0 +1,34 @@
+<?php
+
+$cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.php';
+
+$cfg['suppress_issue_types'] = array_merge(
+	$cfg['suppress_issue_types'],
+	[
+		// Suppress issue types that currently exist in the codebase.
+		'MediaWikiNoEmptyIfDefined',
+		'PhanImpossibleTypeComparison',
+		'PhanParamTooFewInPHPDoc',
+		'PhanPluginUnreachableCode',
+		'PhanPossiblyUndeclaredVariable',
+		'PhanRedundantCondition',
+		'PhanSuspiciousWeakTypeComparison',
+		'PhanTypeMagicVoidWithReturn',
+		'PhanTypeMismatchArgument',
+		'PhanTypeMismatchArgumentProbablyReal',
+		'PhanTypeMismatchDefault',
+		'PhanTypeMissingReturn',
+		'PhanTypeMismatchReturnProbablyReal',
+		'PhanUndeclaredClassMethod',
+		'PhanUndeclaredExtendedClass',
+		'PhanUndeclaredMethod',
+		'PhanUndeclaredProperty',
+		'PhanUndeclaredTypeParameter',
+		'PhanUndeclaredVariable',
+		'PhanUndeclaredVariableDim',
+		'PhanTypeArraySuspiciousNull',
+		'SecurityCheck-DoubleEscaped',
+	]
+);
+
+return $cfg;
